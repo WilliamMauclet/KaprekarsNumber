10ffe7f7ec72b2821105bb934c8d4fba8c65ad8a
William <William.Mauclet@staff.telenet.be>
2016-09-30 14:29:50 +0200

    Works for any number of digits

--- Kaprekar.py
+++ Kaprekar.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
 
-#first find groups up to 9999
+import sys
+
+digitsNr = 	int(raw_input('Please choose how many digits you want: '))
 
 def initPrototypes():
 	prototypes = set()
-
-	for counter in range(0,9999):
+	
+	global digitsNr
+	prototypesNr = (10**digitsNr)-1
+	
+	for counter in range(0, prototypesNr):
 		prototypes.add(numberToSortedString(counter))
 		
 	return sorted(list(prototypes))
 
 def numberToSortedString(number):
 	numberString = sortString(str(number), True)
-	while len(numberString) < 4:
+	while len(numberString) < digitsNr:
 		numberString = '0' + numberString
 	return numberString
 
 def sortString(given, incrNotDecr):
-	return ''.join(sorted(given),reverse=incrNotDecr)
-
+	return ''.join(sorted(given,reverse=not(incrNotDecr)))
+	
 def printPrototypes(prototypes):
 	global f
 	f.write('------PROTOTYPES---------\n')
@@ -28,13 +33,30 @@ def printPrototypes(prototypes):
 		
 			
 def printMappings(nextPrototypeMaps):
-	global f
+	global f, previousNumberOfPrototypes, numberOfPrototypes, iterationsCount
+	previousNumberOfPrototypes = numberOfPrototypes
+	numberOfPrototypes = countImageCardinality(nextPrototypeMaps)
+
+	printNewMapping(numberOfPrototypes, nextPrototypeMaps)
+
+	if(numberOfPrototypes == previousNumberOfPrototypes):
+		printFinalText()
+		return
+	iterationsCount += 1
+	
+def printNewMapping():
 	f.write('\n------------------NEW MAP---------------\n')	
-	f.write('size image: ' + str(countImageCardinality(nextPrototypeMaps)) + '\n')
+	f.write('size image: ' + str(numberOfPrototypes) + '\n')
 	for protoMap in nextPrototypeMaps:
 		f.write(protoMap[0] + ' -> ' + protoMap[1] + '\n')
-
-
+		
+def printFinalText():
+	global f, iterationsCount, digitsNr
+	f.write('\n------------------DONE---------------\n')
+	f.write('Number of iterations for a Kaprekar number with ' + str(digitsNr) + ' digits is: ' + str(iterationsCount))
+	f.write('\n')
+	f.close()
+	
 def makeCalculation(prototype):
 	high = sortString(prototype, False)
 	low = sortString(prototype, True)
@@ -61,13 +83,13 @@ def performMapping():
 	
 def makeNewList(prototypeMaps):
 	return sorted(list(set([protoMap[1] for protoMap in prototypeMaps])))
-
+	
 	
 ####################################### FLOW #########################
 
 prototypes = initPrototypes()
 		
-f = open('prototypes.txt', 'w')
+f = open(str(digitsNr) + '-Digits-Prototypes.txt', 'w')
 printPrototypes(prototypes)
 	
 #Now calculate mappings breadth-first	
@@ -75,8 +97,13 @@ nextPrototypeMaps = []
 for proto in prototypes:
 	nextPrototypeMaps.append((None, proto))
 
-for i in range(1,8):
+# hasta quel numero de grupos sigue el mismo
+previousNumberOfPrototypes = 9999999999
+numberOfPrototypes = 1999999
+iterationsCount = 0
+while previousNumberOfPrototypes != numberOfPrototypes:
 	performMapping()
+
 
 
 
