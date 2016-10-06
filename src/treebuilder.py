136189b2e2ab39b705cdfb96758218035151b75b
William <William.Mauclet@staff.telenet.be>
2016-10-06 11:46:15 +0200

    prototypemapping.py calculates loops and treebuilder.py correctly builds tree.

--- src/treebuilder.py
+++ src/treebuilder.py
@@ -1,23 +1,67 @@
 ## This class traverses the mappings' linked list and builds a number of trees from the mappings.
 ## The trees are a list of dictionaries
 
-def buildTree(prototypeMapping, tree=None):
-	if prototypeMapping.iterationCount == 1:
-		newTree = initialTree(prototypeMapping)
+def buildTree(prototypeMapping):
+	tree = initialTree(prototypeMapping)
+	if not hasattr(prototypeMapping, 'successor'):
+		return tree
 	else:
+		return growTree(prototypeMapping.successor, tree)
+	
+def initialTree(prototypeMapping):
+	firstDictionary = {}
+	for protoMap in prototypeMapping.prototypeMaps:
+		if protoMap[1] not in firstDictionary:
+			firstDictionary[protoMap[1]] = [protoMap[0]]
+		else:
+			firstDictionary[protoMap[1]].append(protoMap[0])
+	return firstDictionary
+	
+def growTree(prototypeMapping, tree):
+	if hasattr(prototypeMapping, 'successor'):
 		newTree = {}
 		for protoMap in prototypeMapping.prototypeMaps:
+			payload = tree[protoMap[0]]
 			if protoMap[1] not in newTree:
-				newTree[protoMap[1]] = [protoMap[0]]
+				newTree[protoMap[1]] = [[protoMap[0], payload]]
 			else:
-				newTree[protoMap[1]].append(protoMap[0])
-	if hasattr(prototypeMapping, 'successor'):
-		return buildTree(prototypeMapping.successor, newTree)
+				newTree[protoMap[1]].append([protoMap[0], payload])
+		return growTree(prototypeMapping.successor, newTree)
 	else:
-		return newTree
-		
+		return tree
 
-def initialTree(prototypeMapping):
-	return dict( (x[1],x[0]) for x in prototypeMapping.prototypeMaps)
+
+	
+# HOW A TREE SHOULD BE BUILT:  => always dictionary
+# 1->2		3->1		
+# 2->3		4->2		
+# 3->1		2->3		
+# 4->2
+# 5->4
+
+# >>>
+
+# {
+	# 1:[3]
+	# 2:[1,4],
+	# 3:[2],
+	# 4:[5]
+# }
+
+# >>>
+
+# {
+	# 1:[3,[2]],
+	# 2:[4,[5]],
+	# 3:[2,[1,4]]
+# }
+
+# >>>
+
+# [1,[3,[2]]]
+# [2,[4,[5]]]
+# [3,[2,[1,4]]]
+
+# ADD LOOPS
 
 	
